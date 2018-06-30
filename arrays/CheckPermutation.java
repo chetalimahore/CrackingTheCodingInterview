546c7c599f2db34f451ab7a091da3826a56c95d4
2D\chetali.mahore <chetali.mahore@slalom.com>
2018-06-30 15:47:39 -0400

    Check if a string is a permutation of the other

---
+++ arrays/CheckPermutation.java
@@ -0,0 +1,57 @@
+package com.practice.arrays;
+
+import java.util.*;
+
+public class CheckPermutation {
+
+	/*O(n) space complexity using Hashmap*/
+	
+	public static boolean checkPermutation(String one, String two){
+		if(one.length() != two.length()) {
+			return false;
+		}
+		
+		HashMap<Character, Integer> hmap = new HashMap<Character, Integer>();
+		
+		for(int i = 0 ; i < one.length() ; i++) {
+			int value;
+			char key;
+			key = one.charAt(i);
+		     if(hmap.containsKey(key)) {
+		    	value =  hmap.get(key);
+		    	value++;
+		    	hmap.put(key, value);
+		     }
+		     else {
+		    	 hmap.put(key, 1);
+		     }
+		}
+		
+		for(int i = 0 ; i < two.length() ; i++) {
+			int value;
+			char key;
+			key = two.charAt(i);
+		     if(hmap.containsKey(key)) {
+		    	value =  hmap.get(key);
+		    	value--;
+		    	if(value < 0) {
+		    		return false;
+		    	}else {
+		    		hmap.put(key, value);
+		    	}
+		     }
+		     else {
+		    	return false;
+		     }
+		}
+		
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//reverse - xsmkmsksncghdsca
+		System.out.println(checkPermutation("acsdhgcnsksmkmsx","xsmkmsksncghdsca"));
+	}
+
+}
