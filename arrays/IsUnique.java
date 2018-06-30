35f3bd5f89dec1f91db9be0e8cff96bf1065fe85
2D\chetali.mahore <chetali.mahore@slalom.com>
2018-06-30 14:38:27 -0400

    To check whether a string is unique or not

---
+++ arrays/IsUnique.java
@@ -0,0 +1,40 @@
+package com.practice.arrays;
+
+import java.util.*;
+
+public class IsUnique {
+	
+	/*O(n^2) approach to find whether the string is unique or not without any new data structure*/
+	
+	public static boolean isUnique(String input) {
+		for(int i = 0 ; i < input.length() ; i++) {
+			for(int j = 0 ; j < input.length() ; j++) {
+				if(input.charAt(i) == input.charAt(j) && (i != j)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	/*O(n logn ) approach to find whether is string is unique or not by sorting the array*/
+	
+	public static boolean isUniqueSort(String input) {
+		char str[] = input.toCharArray();
+		Arrays.sort(str);
+
+		for(int i = 0 ; i < str.length - 1; i++) {
+			if( str[i] == str[i+1]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(isUnique("!abc123@."));
+		System.out.println(isUniqueSort("abcfd"));
+	}
+
+}
