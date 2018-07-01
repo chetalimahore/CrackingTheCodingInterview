546abed9d8df5226b905a68a80526984a325e50e
2D\chetali.mahore <chetali.mahore@slalom.com>
2018-07-01 17:58:02 -0400

    To perform string compression using counts of repeated characters

---
+++ arrays/StringCompression.java
@@ -0,0 +1,42 @@
+/*
+ * @author - Chetali Mahore
+ * *
+ */
+
+package com.practice.arrays;
+
+public class StringCompression {
+
+	/*O(n^2) approach for string compression*/
+	
+	public static String stringCompression(String input) {
+		int i = 0, j = 1, count = 0;
+		StringBuilder build = new StringBuilder();
+		while(i < input.length()) {
+			count = 1;
+			while(j < input.length()) {
+				if(input.charAt(i) == input.charAt(j)) {
+					count++;
+					j++;
+				}
+				else {
+					break;
+				}
+			}
+			build.append(input.charAt(i)).append(count);
+			if(j == input.length()) {
+				break;
+			}
+			else {
+				i = j ;
+				j = j+1;
+			}
+		}
+		
+		return build.toString();
+	}
+	
+	public static void main(String args[]) {
+		System.out.println(stringCompression("aabcccccaaad"));
+	}
+}
